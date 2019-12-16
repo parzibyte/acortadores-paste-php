89a219893b98cad1afadfcfb91cf93af614992fa
Luis Cabrera Benito <parzibyte@gmail.com>
2019-12-15 19:43:17 -0600

    Formulario ajustes

---
+++ app/Controladores/ControladorAjustes.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parzibyte
+ * Date: 12/15/2019
+ * Time: 7:34 PM
+ */
+
+namespace Parzibyte\Controladores;
+class ControladorAjustes
+{
+    public static function index()
+    {
+        return view("ajustes/ajustes");
+    }
+}
